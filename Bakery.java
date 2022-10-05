9e3485ba90693c3c01b1a0863b16d40f0e81eb5b
Ethan <ethansaddlerm@gmail.com>
2022-10-05 09:53:10 -0400

    okokko!

--- Bakery.java
+++ Bakery.java
@@ -1,1 +1,11 @@
+public class Bakery {
+    private String product;
+    private int quantity;
+    private float cost;
 
+    public Bakery(product, quantity, cost) {
+        this.product = product;
+        this.quantity = quantity;
+        this.cost = cost;
+    }
+}
