91c81b259469b3c6727c88b8419e2f2e6766900f
Ethan <ethansaddlerm@gmail.com>
2022-10-05 20:05:11 -0400

    almost done cart

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,1 +1,21 @@
+public class ShoppingCart {
+    private Dairy[] dairyItems;
+    private Bakery[] bakeryItems;
 
+    public ShoppingCart(dairyItems, bakeryItems) {
+        this.dairyItems = dairyItems;
+        this.bakeryItems = bakeryItems;
+    }
+    public ShoppingCart() {
+        this.bakeryItems = new Bakery[2];
+        this.bakeryItems[0] = Bakery("Bread", 1, 5.99);
+        this.dairyItems = new Dairy[4];
+    }
+    public static void addDairyItemAtIndex(index, Dairy) {
+        if (dairyItems[index] != null) {
+            System.out.printf("There was %")
+        }
+        dairyItems[index] = Dairy;
+
+    }
+}
